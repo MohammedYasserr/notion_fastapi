250be348dfa86b1b3e35067abdc08c50597c0440
MohammedYasserr <bmbmyasser@gmail.com>
2024-06-08 00:50:03 +0200

    Validating request using pydantic

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from pydantic import BaseModel
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+
+
+class RequestHandler(BaseModel):
+    """
+    Class Handler that handles the request.
+    
+    """
+    task : str
 
 
 @app.get("/")
