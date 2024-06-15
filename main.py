beccae27c50f71534c1d2e8f17339c4587946591
MohammedYasserr <bmbmyasser@gmail.com>
2024-06-15 13:49:11 +0200

    fixing the setup

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id):
-    return {"item_id" : item_id}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
