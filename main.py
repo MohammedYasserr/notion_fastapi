8f9076df7ca7ff23f77850414cb8ca692bf5bb83
MohammedYasserr <bmbmyasser@gmail.com>
2024-06-17 17:06:11 +0200

    OAuth2PasswordBearer Depencey injection

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 import sys
 from Validations import schema
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 
 @app.get("/tasks/" , response_model=list[schema.Task])
-async def read_tasks(ski):
+async def read_tasks(skip: int = 10, limit: int =10, db: Session= Depends(get_db), token: str= Depends(oauth2_scheme)):
     return {"items": "These are the items"}
