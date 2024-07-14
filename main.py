ce616c3b818f64d637ce4816ed575ec7237face3
MohammedYasserr <bmbmyasser@gmail.com>
2024-07-14 02:22:59 +0200

    creating the get_db() function

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from typing import List
 from sqlalchemy.orm import Session
 import datetime
 from pydantic import BaseModel
-
 import Models
 from Validations import schema
-
 from databases import engine,sessionLocal
 import models
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine) 
 
 
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 
-
-
-@app.get("/tasks/" , response_model=list[schema.Task])
-async def read_tasks(skip: int = 10, limit: int =10, db: Session= Depends(get_db), token: str= Depends(oauth2_scheme)):
-    return {"items": "These are the items"}
